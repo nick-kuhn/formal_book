6b1885382917110e4dd6ff55bc367999751cac08
nick-kuhn <46423253+nick-kuhn@users.noreply.github.com>
2022-10-07 22:14:48 +0200

    some tries at stuff

--- src/chapters/06_Every_finite_division_ring_is_a_field.lean
+++ src/chapters/06_Every_finite_division_ring_is_a_field.lean
@@ -25,6 +25,7 @@ import algebra.group.conj
 import linear_algebra.finite_dimensional
 import linear_algebra.basis
 import data.polynomial.basic
+import ring_theory.polynomial.cyclotomic.basic
 
 open finset subring polynomial
 open_locale big_operators nat polynomial
@@ -41,16 +42,30 @@ This is a TODO in `ring_theory.integral_domain`.
 
 --Define cyclotomic polynomials and check their basic properties
 
+--We use a recursive definition, see here: https://exlean.org/well-founded-recursion/
+noncomputable def phi_rec : Π  (n : ℕ) (h : Π (d : ℕ), d < n → ℤ[X]), ℤ[X]
+| 0 _ := 0
+| 1 _ := X + 1
+| (n + 1) h := div_by_monic (X ^ (n + 1) - 1 )
+    (∏ i in (Icc 1 n), if h_i : i< n + 1 then h i h_i else 1)
 
-def phi (d : ℕ) : ℤ[X] := sorry
 
-lemma phi_div (n : ℕ) : phi n ∣ X ^ n - 1 :=
+noncomputable def phi := well_founded.fix nat.lt_wf phi_rec
+
+example (r n: ℕ) : (r ∈ range n) → r < n := mem_range.mp
+
+lemma phi_div (n : ℕ) (h : n ≠ 0): phi n ∣ X ^ n - 1 :=
 begin
-  sorry,
+  cases n,
+    exact absurd (eq.refl 0) h,
+  set m := n.succ,
+  dsimp only [has_dvd.dvd],
+    use (∏ i in Icc 1 n, phi i),
+
 end
 
 lemma phi_div_2 (n : ℕ) (k : ℕ) (h₂ : k ∣ n) (h₃ : k ≠ n) :
-  phi n ∣ div_by_monic (X ^ n - 1) (X ^ k - 1) :=
+  (phi n) * (X ^ k - 1) ∣ (X ^ n - 1)  :=
 begin
   sorry,
 end
@@ -76,24 +91,36 @@ begin
 
   --conjugacy classes with more than one element
   let S := {A : conj_classes Rˣ | fintype.card A.carrier > 1}.to_finset,
-  let n_k : S → ℕ := λ A, fintype.card
+  let n_k : ( conj_classes Rˣ) → ℕ := λ A, fintype.card
     (set.centralizer ({(quotient.out' (A : conj_classes Rˣ))} : set Rˣ)),
 
   --class  formula
   suffices : q ^ n - 1 = q - 1  + ∑ A in S, (q ^ n - 1) / (q ^ (fintype.card A.carrier) - 1), by
 
-  { have : ∀ A : S, (n_k A ∣ n) := sorry,
+  { have n_k_dvd : ∀ A : S, (n_k A ∣ n) := sorry,
 
-  --rest of proof
+    have div₁ : (phi n).eval q ∣ q ^ n - 1, by
+    { sorry, },
 
-  sorry,
+    have : (phi n).eval q ∣ ∑ A in S, (q ^ n - 1) / (q ^ (fintype.card A.carrier) - 1), by
+    { apply dvd_sum,
+      intros A h_A,
+      apply int.dvd_div_of_mul_dvd,
+      let k := n_k A,
+      have :=eval_dvd (phi_div_2 n (n_k A) (n_k_dvd ⟨A, h_A⟩) sorry),
+      convert this, sorry, sorry, sorry,},
+
+
+  have : cyclotomic
+
+
 
   },
   --proof of class  formula
 
 
-
-  all_goals {sorry},
+  sorry,
 end
 
+#check polynomial.eval
 end wedderburn
